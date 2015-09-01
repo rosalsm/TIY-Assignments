3bc4020b9e38fc59b31aa7dd11cd9b5cf6d24c73
rosa <r215130@gmail.com>
2015-09-01 17:06:26 -0400

    working in addition etc

---
+++ BasicCalculator.js
@@ -0,0 +1,11 @@
+console.assert(add(0, 0), /* FILL ME IN */);
+console.assert(add(0, 1), /* FILL ME IN */);
+console.assert(add(0, 2), /* FILL ME IN */);
+
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+function add(A, B){
+} // END add
