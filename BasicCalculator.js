d22c4faf2bdbeece610f7e6f22168ad0af74f37f
rosa <r215130@gmail.com>
2015-09-01 23:00:32 -0400

    new changes

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,410 +1,252 @@
-console.assert(add(0, 0), /* adding 0 */);
-console.assert(add(0, 1), /* adding 1 */);
-console.assert(add(0, 2), /* adding 2 */);,
-console.assert(add(0, 3), /* adding 3 */);
-console.assert(add(0, 4), /* adding 4 */);
-console.assert(add(0, 5), /* adding 5 */);
-console.assert(add(0, 6), /* adding 6 */);
-console.assert(add(0, 7), /* adding 7 */);
-console.assert(add(0, 8), /* adding 8 */);
-console.assert(add(0, 9), /* adding 9 */);
+function add(A, B) {
+  return A + B;
+};
+
+console.assert(add(0, 0) === 0);
+console.assert(add(0, 1) === 1);
+console.assert(add(0, 2) === 2);
+console.assert(add(0, 3) === 3);
+console.assert(add(0, 4) === 4);
+console.assert(add(0, 5) === 5);
+console.assert(add(0, 6) === 6);
+console.assert(add(0, 7) === 7);
+console.assert(add(0, 8) === 8);
+console.assert(add(0, 9) === 9);
 
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
  * @return {Number} sum of A and B
  */
-function add(A, B){
-  return A + B
-} // END add
 
-console.assert(add(1, 0),/*adding 0 */);
-console.assert(add(1, 1), /* adding 1 */);
-console.assert(add(1, 2), /* adding 2 */);,
-console.assert(add(1, 3), /* adding 3 */);
-console.assert(add(1, 4), /* adding 4 */);
-console.assert(add(1, 5), /* adding 5 */);
-console.assert(add(1, 6), /* adding 6 */);
-console.assert(add(1, 7), /* adding 7 */);
-console.assert(add(1, 8), /* adding 8 */);
-console.assert(add(1, 9), /* adding 9 */);
+ function add(A, B) {
+   return A + B;
+ };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
+ console.assert(add(1, 0) === 1);
+ console.assert(add(1, 1) === 2);
+ console.assert(add(1, 2) === 3);
+ console.assert(add(1, 3) === 4);
+ console.assert(add(1, 4) === 5);
+ console.assert(add(1, 5) === 6);
+ console.assert(add(1, 6) === 7);
+ console.assert(add(1, 7) === 8);
+ console.assert(add(1, 8) === 9);
+ console.assert(add(1, 9) === 10);
 
-console.assert(add(2, 0),/*adding 0 */);
-console.assert(add(2, 1), /* adding 1 */);
-console.assert(add(2, 2), /* adding 2 */);,
-console.assert(add(2, 3), /* adding 3 */);
-console.assert(add(2, 4), /* adding 4 */);
-console.assert(add(2, 5), /* adding 5 */);
-console.assert(add(2, 6), /* adding 6 */);
-console.assert(add(2, 7), /* adding 7 */);
-console.assert(add(2, 8), /* adding 8 */);
-console.assert(add(2, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(3, 0),/*adding 0 */);
-console.assert(add(3, 1), /* adding 1 */);
-console.assert(add(3, 2), /* adding 2 */);,
-console.assert(add(3, 3), /* adding 3 */);
-console.assert(add(3, 4), /* adding 4 */);
-console.assert(add(3, 5), /* adding 5 */);
-console.assert(add(3, 6), /* adding 6 */);
-console.assert(add(3, 7), /* adding 7 */);
-console.assert(add(3, 8), /* adding 8 */);
-console.assert(add(3, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(4, 0),/*adding 0 */);
-console.assert(add(4, 1), /* adding 1 */);
-console.assert(add(4, 2), /* adding 2 */);,
-console.assert(add(4, 3), /* adding 3 */);
-console.assert(add(4, 4), /* adding 4 */);
-console.assert(add(4, 5), /* adding 5 */);
-console.assert(add(4, 6), /* adding 6 */);
-console.assert(add(4, 7), /* adding 7 */);
-console.assert(add(4, 8), /* adding 8 */);
-console.assert(add(4, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(5, 0),/*adding 0 */);
-console.assert(add(5, 1), /* adding 1 */);
-console.assert(add(5, 2), /* adding 2 */);,
-console.assert(add(5, 3), /* adding 3 */);
-console.assert(add(5, 4), /* adding 4 */);
-console.assert(add(5, 5), /* adding 5 */);
-console.assert(add(5, 6), /* adding 6 */);
-console.assert(add(5, 7), /* adding 7 */);
-console.assert(add(5, 8), /* adding 8 */);
-console.assert(add(5, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(6, 0),/*adding 0 */);
-console.assert(add(6, 1), /* adding 1 */);
-console.assert(add(6, 2), /* adding 2 */);,
-console.assert(add(6, 3), /* adding 3 */);
-console.assert(add(6, 4), /* adding 4 */);
-console.assert(add(6, 5), /* adding 5 */);
-console.assert(add(6, 6), /* adding 6 */);
-console.assert(add(6, 7), /* adding 7 */);
-console.assert(add(6, 8), /* adding 8 */);
-console.assert(add(6, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(7, 0),/*adding 0 */);
-console.assert(add(7, 1), /* adding 1 */);
-console.assert(add(7, 2), /* adding 2 */);,
-console.assert(add(7, 3), /* adding 3 */);
-console.assert(add(7, 4), /* adding 4 */);
-console.assert(add(7, 5), /* adding 5 */);
-console.assert(add(7, 6), /* adding 6 */);
-console.assert(add(7, 7), /* adding 7 */);
-console.assert(add(7, 8), /* adding 8 */);
-console.assert(add(7, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(8, 0),/*adding 0 */);
-console.assert(add(8, 1), /* adding 1 */);
-console.assert(add(8, 2), /* adding 2 */);,
-console.assert(add(8, 3), /* adding 3 */);
-console.assert(add(8, 4), /* adding 4 */);
-console.assert(add(8, 5), /* adding 5 */);
-console.assert(add(8, 6), /* adding 6 */);
-console.assert(add(8, 7), /* adding 7 */);
-console.assert(add(8, 8), /* adding 8 */);
-console.assert(add(8, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
-
-console.assert(add(9, 0),/*adding 0 */);
-console.assert(add(9, 1), /* adding 1 */);
-console.assert(add(9, 2), /* adding 2 */);,
-console.assert(add(9, 3), /* adding 3 */);
-console.assert(add(9, 4), /* adding 4 */);
-console.assert(add(9, 5), /* adding 5 */);
-console.assert(add(9, 6), /* adding 6 */);
-console.assert(add(9, 7), /* adding 7 */);
-console.assert(add(9, 8), /* adding 8 */);
-console.assert(add(9, 9), /* adding 9 */);
-
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} sum of A and B
- */
-function add(A, B){
-  return A + B
-} // END add
+ /**
+  * @param {Number} A to add to B
+  * @param {Number} B to add to A
+  * @return {Number} sum of A and B
+  */
 
 
+  function add(A, B) {
+    return A + B;
+  };
 
+  console.assert(add(2, 0) === 2);
+  console.assert(add(2, 1) === 3);
+  console.assert(add(2, 2) === 4);
+  console.assert(add(2, 3) === 5);
+  console.assert(add(2, 4) === 6);
+  console.assert(add(2, 5) === 7);
+  console.assert(add(2, 6) === 8);
+  console.assert(add(2, 7) === 9);
+  console.assert(add(2, 8) === 10);
+  console.assert(add(2, 9) === 11);
 
+  /**
+   * @param {Number} A to add to B
+   * @param {Number} B to add to A
+   * @return {Number} sum of A and B
+   */
 
+   function add(A, B) {
+     return A + B;
+   };
 
-console.assert(sub(9, 0), /* substracting 0 */);
-console.assert(sub(9, 1), /* substracting 1 */);
-console.assert(sub(9, 2), /* substracting 2 */);
-console.assert(sub(9, 3), /* substracting 3 */);
-console.assert(sub(9, 4), /* substracting 4 */);
-console.assert(sub(9, 5), /* substracting 5 */);
-console.assert(sub(9, 6), /* substracting 6 */);
-console.assert(sub(9, 7), /* substracting 7 */);
-console.assert(sub(9, 8), /* substracting 8 */);
-console.assert(sub(9, 9), /* substracting 9 */);
+   console.assert(add(3, 0) === 3);
+   console.assert(add(3, 1) === 4);
+   console.assert(add(3, 2) === 5);
+   console.assert(add(3, 3) === 6);
+   console.assert(add(3, 4) === 7);
+   console.assert(add(3, 5) === 8);
+   console.assert(add(3, 6) === 9);
+   console.assert(add(3, 7) === 10);
+   console.assert(add(3, 8) === 11);
+   console.assert(add(3, 9) === 12);
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+   /**
+    * @param {Number} A to add to B
+    * @param {Number} B to add to A
+    * @return {Number} sum of A and B
+    */
 
-console.assert(sub(8, 0), /* substracting 0 */);
-console.assert(sub(8, 1), /* substracting 1 */);
-console.assert(sub(8, 2), /* substracting 2 */);
-console.assert(sub(8, 3), /* substracting 3 */);
-console.assert(sub(8, 4), /* substracting 4 */);
-console.assert(sub(8, 5), /* substracting 5 */);
-console.assert(sub(8, 6), /* substracting 6 */);
-console.assert(sub(8, 7), /* substracting 7 */);
-console.assert(sub(8, 8), /* substracting 8 */);
-console.assert(sub(8, 9), /* substracting 9 */);
+    function add(A, B) {
+      return A + B;
+    };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+    console.assert(add(4, 0) === 4);
+    console.assert(add(4, 1) === 5);
+    console.assert(add(4, 2) === 6);
+    console.assert(add(4, 3) === 7);
+    console.assert(add(4, 4) === 8);
+    console.assert(add(4, 5) === 9);
+    console.assert(add(4, 6) === 10);
+    console.assert(add(4, 7) === 11);
+    console.assert(add(4, 8) === 12);
+    console.assert(add(4, 9) === 13);
 
-console.assert(sub(7, 0), /* substracting 0 */);
-console.assert(sub(7, 1), /* substracting 1 */);
-console.assert(sub(7, 2), /* substracting 2 */);
-console.assert(sub(7, 3), /* substracting 3 */);
-console.assert(sub(7, 4), /* substracting 4 */);
-console.assert(sub(7, 5), /* substracting 5 */);
-console.assert(sub(7, 6), /* substracting 6 */);
-console.assert(sub(7, 7), /* substracting 7 */);
-console.assert(sub(7, 8), /* substracting 8 */);
-console.assert(sub(7, 9), /* substracting 9 */);
+    /**
+     * @param {Number} A to add to B
+     * @param {Number} B to add to A
+     * @return {Number} sum of A and B
+     */
 
+     function add(A, B) {
+       return A + B;
+     };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+     console.assert(add(5, 0) === 5);
+     console.assert(add(5, 1) === 6);
+     console.assert(add(5, 2) === 7);
+     console.assert(add(5, 3) === 8);
+     console.assert(add(5, 4) === 9);
+     console.assert(add(5, 5) === 10);
+     console.assert(add(5, 6) === 11);
+     console.assert(add(5, 7) === 12);
+     console.assert(add(5, 8) === 13);
+     console.assert(add(5, 9) === 14);
 
-console.assert(sub(6, 0), /* substracting 0 */);
-console.assert(sub(6, 1), /* substracting 1 */);
-console.assert(sub(6, 2), /* substracting 2 */);
-console.assert(sub(6, 3), /* substracting 3 */);
-console.assert(sub(6, 4), /* substracting 4 */);
-console.assert(sub(6, 5), /* substracting 5 */);
-console.assert(sub(6, 6), /* substracting 6 */);
-console.assert(sub(6, 7), /* substracting 7 */);
-console.assert(sub(6, 8), /* substracting 8 */);
-console.assert(sub(6, 9), /* substracting 9 */);
+     /**
+      * @param {Number} A to add to B
+      * @param {Number} B to add to A
+      * @return {Number} sum of A and B
+      */
 
+      function sub(A, B) {
+        return A - B;
+      };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+      console.assert(sub(1, 0) === 1);
+      console.assert(sub(1, 1) === 0);
+      console.assert(sub(1, 2) === -1);
+      console.assert(sub(1, 3) === -2);
+      console.assert(sub(1, 4) === -3);
+      console.assert(sub(1, 5) === -4);
+      console.assert(sub(1, 6) === -5);
+      console.assert(sub(1, 7) === -6);
+      console.assert(sub(1, 8) === -7);
+      console.assert(sub(1, 9) === -8);
 
-console.assert(sub(5, 0), /* substracting 0 */);
-console.assert(sub(5, 1), /* substracting 1 */);
-console.assert(sub(5, 2), /* substracting 2 */);
-console.assert(sub(5, 3), /* substracting 3 */);
-console.assert(sub(5, 4), /* substracting 4 */);
-console.assert(sub(5, 5), /* substracting 5 */);
-console.assert(sub(5, 6), /* substracting 6 */);
-console.assert(sub(5, 7), /* substracting 7 */);
-console.assert(sub(5, 8), /* substracting 8 */);
-console.assert(sub(5, 9), /* substracting 9 */);
+      /**
+       * @param {Number} A to add to B
+       * @param {Number} B to add to A
+       * @return {Number} sum of A and B
+       */
 
+       function sub(A, B) {
+         return A - B;
+       };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+       console.assert(sub(2, 0) === 2);
+       console.assert(sub(2, 1) === 1);
+       console.assert(sub(2, 2) === 0);
+       console.assert(sub(2, 3) === -1);
+       console.assert(sub(2, 4) === -2);
+       console.assert(sub(2, 5) === -3);
+       console.assert(sub(2, 6) === -4);
+       console.assert(sub(2, 7) === -5);
+       console.assert(sub(2, 8) === -6);
+       console.assert(sub(2, 9) === -7);
 
-console.assert(sub(4, 0), /* substracting 0 */);
-console.assert(sub(4, 1), /* substracting 1 */);
-console.assert(sub(4, 2), /* substracting 2 */);
-console.assert(sub(4, 3), /* substracting 3 */);
-console.assert(sub(4, 4), /* substracting 4 */);
-console.assert(sub(4, 5), /* substracting 5 */);
-console.assert(sub(4, 6), /* substracting 6 */);
-console.assert(sub(4, 7), /* substracting 7 */);
-console.assert(sub(4, 8), /* substracting 8 */);
-console.assert(sub(4, 9), /* substracting 9 */);
+       /**
+        * @param {Number} A to add to B
+        * @param {Number} B to add to A
+        * @return {Number} sum of A and B
+        */
 
+        function sub(A, B) {
+          return A - B;
+        };
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+        console.assert(sub(3, 0) === 3);
+        console.assert(sub(3, 1) === 2);
+        console.assert(sub(3, 2) === 1);
+        console.assert(sub(3, 3) === 0);
+        console.assert(sub(3, 4) === -1);
+        console.assert(sub(3, 5) === -2);
+        console.assert(sub(3, 6) === -3);
+        console.assert(sub(3, 7) === -4);
+        console.assert(sub(3, 8) === -5);
+        console.assert(sub(3, 9) === -6);
 
-console.assert(sub(3, 0), /* substracting 0 */);
-console.assert(sub(3, 1), /* substracting 1 */);
-console.assert(sub(3, 2), /* substracting 2 */);
-console.assert(sub(3, 3), /* substracting 3 */);
-console.assert(sub(3, 4), /* substracting 4 */);
-console.assert(sub(3, 5), /* substracting 5 */);
-console.assert(sub(3, 6), /* substracting 6 */);
-console.assert(sub(3, 7), /* substracting 7 */);
-console.assert(sub(3, 8), /* substracting 8 */);
-console.assert(sub(3, 9), /* substracting 9 */);
+        /**
+         * @param {Number} A to add to B
+         * @param {Number} B to add to A
+         * @return {Number} sum of A and B
+         */
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+         function sub(A, B) {
+           return A - B;
+         };
 
-console.assert(sub(2, 0), /* substracting 0 */);
-console.assert(sub(2, 1), /* substracting 1 */);
-console.assert(sub(2, 2), /* substracting 2 */);
-console.assert(sub(2, 3), /* substracting 3 */);
-console.assert(sub(2, 4), /* substracting 4 */);
-console.assert(sub(2, 5), /* substracting 5 */);
-console.assert(sub(2, 6), /* substracting 6 */);
-console.assert(sub(2, 7), /* substracting 7 */);
-console.assert(sub(2, 8), /* substracting 8 */);
-console.assert(sub(2, 9), /* substracting 9 */);
+         console.assert(sub(4, 0) === 4);
+         console.assert(sub(4, 1) === 3);
+         console.assert(sub(4, 2) === 2);
+         console.assert(sub(4, 3) === 1);
+         console.assert(sub(4, 4) === 0);
+         console.assert(sub(4, 5) === -1);
+         console.assert(sub(4, 6) === -2);
+         console.assert(sub(4, 7) === -3);
+         console.assert(sub(4, 8) === -4);
+         console.assert(sub(4, 9) === -5);
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+         /**
+          * @param {Number} A to add to B
+          * @param {Number} B to add to A
+          * @return {Number} sum of A and B
+          */
 
-console.assert(sub(1, 0), /* substracting 0 */);
-console.assert(sub(1, 1), /* substracting 1 */);
-console.assert(sub(1, 2), /* substracting 2 */);
-console.assert(sub(1, 3), /* substracting 3 */);
-console.assert(sub(1, 4), /* substracting 4 */);
-console.assert(sub(1, 5), /* substracting 5 */);
-console.assert(sub(1, 6), /* substracting 6 */);
-console.assert(sub(1, 7), /* substracting 7 */);
-console.assert(sub(1, 8), /* substracting 8 */);
-console.assert(sub(1, 9), /* substracting 9 */);
+          function sub(A, B) {
+            return A - B;
+          };
 
+          console.assert(sub(5, 0) === 5);
+          console.assert(sub(5, 1) === 4);
+          console.assert(sub(5, 2) === 3);
+          console.assert(sub(5, 3) === 2);
+          console.assert(sub(5, 4) === 1);
+          console.assert(sub(5, 5) === 0);
+          console.assert(sub(5, 6) === -1);
+          console.assert(sub(5, 7) === -2);
+          console.assert(sub(5, 8) === -3);
+          console.assert(sub(5, 9) === -4);
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+          /**
+           * @param {Number} A to add to B
+           * @param {Number} B to add to A
+           * @return {Number} sum of A and B
+           */
 
-console.assert(sub(0, 0), /* substracting 0 */);
-console.assert(sub(0, 1), /* substracting 1 */);
-console.assert(sub(0, 2), /* substracting 2 */);
-console.assert(sub(0, 3), /* substracting 3 */);
-console.assert(sub(0, 4), /* substracting 4 */);
-console.assert(sub(0, 5), /* substracting 5 */);
-console.assert(sub(0, 6), /* substracting 6 */);
-console.assert(sub(0, 7), /* substracting 7 */);
-console.assert(sub(0, 8), /* substracting 8 */);
-console.assert(sub(0, 9), /* substracting 9 */);
+           function multiply(A, B) {
+             return A * B;
+           };
 
+           console.assert(multiply(0, 0) === 0);
+           console.assert(multiply(0, 1) === 0);
+           console.assert(multiply(0, 2) === 0);
+           console.assert(multiply(0, 3) === 0);
+           console.assert(multiply(0, 4) === 0);
+           console.assert(multiply(0, 5) === 0);
+           console.assert(multiply(0, 6) === 0);
+           console.assert(multiply(0, 7) === 0);
+           console.assert(multiply(0, 8) === 0);
+           console.assert(multiply(0, 9) === 0);
 
-/**
- * @param {Number} A to add to B
- * @param {Number} B to add to A
- * @return {Number} diff of A and B
- */
-function minus(A, B){
-  return A - B
-} // END add
+           /**
+            * @param {Number} A to add to B
+            * @param {Number} B to add to A
+            * @return {Number} sum of A and B
+            */
