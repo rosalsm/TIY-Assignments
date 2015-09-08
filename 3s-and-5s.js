a6302c043547866646dd225b051f9a08e1cacf04
rosa <r215130@gmail.com>
2015-09-08 08:20:28 -0400

    done with 3s&5s, fibonacci and rot13

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -1,1 +1,71 @@
+var test = require('mocha').it,
+    expect = require('chai').expect, // Use `expect(X).to.be` et al
+    assert = require('chai').assert;  // OR use `assert.equal` etc
 
+
+test(multOf3s, function(){
+  expect(multOf3s(5)).to.equal(3);
+  expect(multOf3s(6)).to.equal(9);
+  expect(multOf3s (10)).to.equal(18);
+  expect(multOf3s (12)).to.equal(30);
+  expect(multOf3s (0)).to.equal(0);
+  expect(multOf3s (20)).to.equal(63);
+
+}); // END test 3s
+// function for sum of multiples of 3
+//parameter N is a Number
+// var sum is a number and var n
+//returns a number
+
+function multOf3s (N){
+  var sum = 0, n = 0;
+  for (n=0; n<=N; n++){
+    if (n % 3 === 0){
+      sum += n;
+    }
+  }
+  return sum;
+}
+
+test(multOf5s, function(){
+  expect(multOf5s(5)).to.equal(5);
+  expect(multOf5s(6)).to.equal(5);
+  expect(multOf5s (10)).to.equal(15);
+  expect(multOf5s (12)).to.equal(15);
+  expect(multOf5s (0)).to.equal(0);
+  expect(multOf5s (20)).to.equal(50);
+  expect(multOf5s (50)).to.equal(275);
+  expect(multOf5s (1000)).to.equal(100500);
+
+});
+
+function multOf5s (N){
+  var sum = 0, n = 0;
+  for (n=0; n<=N; n++){
+    if (n % 5 === 0){
+      sum += n;
+    }
+  }
+  return sum;
+}
+
+test(multOf3s5s, function(){
+  expect(mult3s5s(5)).to.equal(8);
+  expect(mult3s5s(10)).to.equal(33);
+  expect(mult3s5s(12)).to.equal(45);
+  expect(mult3s5s(15)).to.equal(60);
+  expect(mult3s5s(0)).to.equal(0);
+  expect(mult3s5s(20)).to.equal(98);
+  expect(mult3s5s(50)).to.equal(593);
+});
+
+
+function multOf3s5s (N){
+  var sum = 0, n = 0
+  for (n=0; n<=N; n++){
+    if (n % 3 === 0 || n % 5 === 0){
+      sum += n;
+    }
+  }
+  return sum
+}
--- ROT13.js
+++ ROT13.js
@@ -1,1 +1,80 @@
+/**
+ * The function `reverse` computes the reversal
+ * of a given `String` (don't copy-pasta!)
+ *
+ * @param {String} S to reverse
+ * @return {String}
+ * S is string, i is index, l is letter
+ */
+ //Rosa string to array .split()|reverse array .reverse()| to.string() Maybe???
 
+function reverse(S){
+  var l = ""
+    for (var i = S.length-1; i >= 0; i--);
+      l += S[i];
+      return l
+}
+
+console.assert(reverse("") === ""); // Really?
+
+console.assert(reverse("A") === "A"); // Jerk.
+
+console.assert(reverse("cat") === "tac");
+
+console.assert(reverse("ward") === "draw");
+
+console.assert(reverse("books") === "skoob"); // Rokey, dokey...
+
+console.assert(
+  reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew"
+);
+
+/**
+ * Function `encode` accepts a `String` and produces
+ * the appropriate ROT13 "encoded" version, i.e. every
+ * character in `phrase` is "rotated" ahead by 13 characters.
+ *
+ * @see String.prototype.charCodeAt
+ * @see String.prototype.fromCharCode
+ * @see http://en.wikipedia.org/wiki/ROT13
+ *
+ * // Start with just `phrase`...
+ * @param {String} phrase to encode
+ * // Add `N` in part 2!
+ * // @param {Number} N rotation to apply, default 13
+ * @return {String} encoded with ROT13
+ */
+
+ 
+// Rosa.arggggggggg no time no time to thinkkkkkk
+var v
+function encode(phrase, N ){
+    for (var i = 0, L = ""; i<= phrase.length-1; i++){
+      if (110>= phrase.charCodeAt(i)>=97){
+        return phrase.charCodeAt(i) + N;
+      } else {return phrase.charCodeAt(i) - N;
+        }
+      }
+    L += phrase2.fromCharCode(v)
+    return L
+}
+
+/**
+ * Function `decode` accepts a `phrase` and `N` and
+ * decoded it appropriately, i.e. every _word_ character
+ * in `phrase` is rotated backward by `N` characters.
+ *
+ * @param {String} phrase to decode
+ * @param {Number} N rotation to apply, default 13
+ * @return {String} decoded by ROT-N
+ */
+function decode(phrase, N){
+    // YOUR CODE HERE
+}
+
+// Produce more examples, please...
+console.assert(encode("hello") === "uryyb");
+console.assert(encode("uryyb") === "hello");
+
+console.assert(encode("hello", 2) === "jgnnq")
+console.assert(decode("jgnnq", 2) === "hello")
