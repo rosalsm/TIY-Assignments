598a0df643483abb158d19479216f45418a6edf1
rosa <r215130@gmail.com>
2015-09-09 18:48:11 -0400

    with my comments

---
+++ MultiColumnForm/js/scripts.js
@@ -0,0 +1,12 @@
+/*
+In this case both .html files can do more or less the same.
+We can write in both of them.
+We can open the different choices and click on them, change the
+choise.
+Colors are the same too, the same about text and aparience.
+
+Probably the only difference is about the side of the choices,
+in the case of the changed file it is a little smaller than
+the original one. 
+
+*/
---
+++ NestedAccordion/js/scripts.js
@@ -0,0 +1,11 @@
+/* This is what I see:
+Differences between these two .html files.
+The one without JavaScript is not interactive. It doesn't have any
+movement or changing colors.
+The accordion is not working (opem/close), it is completely expanded.
+The arrows are not interactive in terms of moving or changing colors
+either.
+
+Like we just deleted and changed the <script> information,
+that gives me the idea of movement and changing colors are
+effects inside this part of the code. 
