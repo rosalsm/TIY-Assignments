8b00ac848e67952991fd8d9ce2e0e709e8c83a80
rosa <r215130@gmail.com>
2015-09-13 16:08:27 -0400

    first round

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -1,12 +1,26 @@
-/*
-In this case both .html files can do more or less the same.
-We can write in both of them.
-We can open the different choices and click on them, change the
-choise.
-Colors are the same too, the same about text and aparience.
 
-Probably the only difference is about the side of the choices,
-in the case of the changed file it is a little smaller than
-the original one. 
 
-*/
+// In this case, we have a form element and has to be filled with first name,
+// last name and other information.
+// We can see an element <input> it has an attribute type 'text'
+
+//If we store the value of all the elements that have input
+//in their name, we get an array of these elements and their values
+var inputsInfo = document.querySelectorAll('input');
+ console.log(inputsInfo);
+
+
+function information (){
+  
+
+}
+
+
+//var firstName = document.getElementById('first_name');//.value;
+//store the value of the element input in a variable
+
+//function first(){
+  //console.log(firstName.value);
+// }
+//
+// firstName.addEventListener('input', first);
