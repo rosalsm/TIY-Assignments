5ebab337f2fb199375c6e1057f6012d23c65aa41
rosa <r215130@gmail.com>
2015-09-13 19:24:09 -0400

    first round multicolumn

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -6,14 +6,21 @@
 
 //If we store the value of all the elements that have input
 //in their name, we get an array of these elements and their values
-var inputsInfo = document.querySelectorAll('input');
- console.log(inputsInfo);
 
 
-function information (){
-  
 
-}
+// var divInfo = document.querySelector('div.cbp-mc-colum');
+// var inputFirstName = document.getElementById('first-name');
+// console.log(divInfo);
+//
+
+
+// var inputsInfo = document.querySelectorAll('input');
+//  console.log(inputsInfo);
+//
+//
+// function information (){
+//
 
 
 //var firstName = document.getElementById('first_name');//.value;
@@ -24,3 +31,20 @@ function information (){
 // }
 //
 // firstName.addEventListener('input', first);
+
+
+
+//1. Select the element node you want the script to respond to.
+var infoInputs = document.querySelector('form');
+console.log(infoInputs);
+//store the form value. Get the entire form with all the elements
+//first name, last name, email, country,etc.
+
+
+var infoSubmit = document.querySelector('.cbp-mc-submit');
+console.log(infoSubmit);
+//store the value of the element to submit
+//the Dom element to target is infoSubmit and the event is click
+infoSubmit.addEventListener('clik', function(){
+  console.log(infoInputs.item[0].nodevalue);
+});
