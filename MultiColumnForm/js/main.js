4ec36c823042ec8cdcfc423a1fbc3953172fcfc7
rosa <r215130@gmail.com>
2015-09-27 14:20:29 -0400

    work in multicolumn

--- MultiColumnForm/js/main.js
+++ MultiColumnForm/js/main.js
@@ -1,3 +1,42 @@
+//JQUERY
+//1 Select the element we want to get the value from (input, textarea or select)
+//the variable stores the value entry by the user in the input name.
+var $listinputs = $('input:text ').val();
+//console.log($listinputs);
+
+//when I add all inputs, selects and texareas in my var,
+//console.log gives me what I was asking for. I have the right selector.
+
+var $listinputs = $('input:text, select, texarea');
+console.log($listinputs);
+
+// [input#name, input#last-name, input#email,
+// select#country, input#experience, input#size,
+// input#favorite, select, select, prevObject:
+// m.fn.init[1], context: document, selector:
+// "input:text, select, texarea"]
+
+
+//Now I need a var for the submit button. This is going to be
+// the eventlistener for the event click.
+
+var $submitf = $('#submit');
+
+//in JQuery, this how we represent the event listener.
+//Submit input is the trigger for the event click, when it happens, the values
+//in the input, select and textarea will be added to the Nodelist by infoOut.
+
+$submitf.on('click', function(e){
+  e.preventDefault();
+  var $infoOut = $listinputs.val();
+})
+
+
+
+
+
+
+
 
 
 // In this case, we have a form element and has to be filled with first name,
@@ -33,18 +72,18 @@
 // firstName.addEventListener('input', first);
 
 
-
-//1. Select the element node you want the script to respond to.
-var infoInputs = document.querySelector('form');
-console.log(infoInputs);
-//store the form value. Get the entire form with all the elements
-//first name, last name, email, country,etc.
-
-
-var infoSubmit = document.querySelector('.cbp-mc-submit');
-console.log(infoSubmit);
-//store the value of the element to submit
-//the Dom element to target is infoSubmit and the event is click
-infoSubmit.addEventListener('clik', function(){
-  console.log(infoInputs.item[0].nodevalue);
-});
+//JAVASCRIPT
+// //1. Select the element node you want the script to respond to.
+// var infoInputs = document.querySelector('form');
+// console.log(infoInputs);
+// //store the form value. Get the entire form with all the elements
+// //first name, last name, email, country,etc.
+//
+//
+// var infoSubmit = document.querySelector('.cbp-mc-submit');
+// console.log(infoSubmit);
+// //store the value of the element to submit
+// //the Dom element to target is infoSubmit and the event is click
+// infoSubmit.addEventListener('clik', function(){
+//   console.log(infoInputs.item[0].nodevalue);
+// });
