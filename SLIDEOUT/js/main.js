81bd85fbd0645a1a040006ca2557ec15e3631b84
rosa <r215130@gmail.com>
2015-10-09 09:29:14 -0400

    working

--- SLIDEOUT/js/main.js
+++ SLIDEOUT/js/main.js
@@ -3,20 +3,20 @@
 $('div.lovely').on('click',function(){
   console.log('probando');
   $(this).addClass('active');
-  $('section#lovely').addClass('active').siblings().removeClass('active');
+  $('section#lovely').toggleClass('active').siblings().removeClass('active');
 })
 $('div.delicious').on('click',function(){
   console.log('probando 2');
   $(this).addClass('active');
-  $('section#delicious').addClass('active').siblings().removeClass('active');
+  $('section#delicious').toggleClass('active').siblings().removeClass('active');
 })
 $('div.fine').on('click',function(){
   console.log('probando 3');
   $(this).addClass('active');
-  $('section#fine').addClass('active').siblings().removeClass('active');
+  $('section#fine').toggleClass('active').siblings().removeClass('active');
 })
 $('div.mission').on('click',function(){
   console.log('probando mission');
   $(this).addClass('active');
-  $('section#mission').addClass('active').siblings().removeClass('active');
+  $('section#mission').toggleClass('active').siblings().removeClass('active');
 })
